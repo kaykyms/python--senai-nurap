232a542cb08ab3b9e9896ed9cbda4be2e0d5b4bd
kayky <mkayky722@gmail.com>
2024-12-05 11:25:56 -0300

    Add files via upload

---
+++ aula 2/aula2.py
@@ -0,0 +1,6 @@
+print("save")
+nome_usuario = "kayky" # tipo ->str = string
+idade_usuario= 21 # tipo -> int= integer
+altura_usuario= 1.86 #tipo -> float = flutuante
+maior_idade = True #tipo -> bool = boolean
+print("seja Bem-Vindo", nome_usuario,"qual sua idade:",idade_usuario,"quanto de altura voce tem:",altura_usuario,"voce e maior de idade:", maior_idade) 
